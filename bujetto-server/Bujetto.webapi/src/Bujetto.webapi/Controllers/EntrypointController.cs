617b7cf5b8b5924ea904b185c023d670adaa6614
Teodor B <tbardici@gmail.com>
2016-10-15 11:55:08 +0200

    default api route

---
+++ bujetto-server/Bujetto.webapi/src/Bujetto.webapi/Controllers/EntrypointController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bujetto.webapi.Controllers
+{
+    [Route("api")]
+    public class EntrypointController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Bujetto API Running";
+        } 
+    }
+}

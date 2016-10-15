8c36f92782fa164154e9e439488123dbbc5e90eb
Teodor B <tbardici@gmail.com>
2016-10-15 18:01:31 +0200

    a

--- bujetto-server/Bujetto.webapi/src/Bujetto.webapi/Program.cs
+++ bujetto-server/Bujetto.webapi/src/Bujetto.webapi/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Bujetto.webapi
 {
@@ -12,9 +13,14 @@ namespace Bujetto.webapi
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
                 .UseIISIntegration()
                 //.UseUrls("http://0.0.0.0:3000")
                 .UseStartup<Startup>()
